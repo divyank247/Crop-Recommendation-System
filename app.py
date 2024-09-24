a7de18d13774e15e323ca80599429e7fa7b18ee5
Divyanshu Gupta <124454534+divyank247@users.noreply.github.com>
2024-09-25 00:48:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+# flask app
+flask_app = Flask(__name__)
+model = pickle.load(open("randomclf.pkl", "rb"))
+
+@flask_app.route("/")
+def Home():
+    return render_template("index.html")
+
+@flask_app.route("/predict", methods = ["POST"])
+def predict():
+    float_features = [float(x) for x in request.form.values()]
+    features = [np.array(float_features)]
+    prediction = model.predict(features)
+    return render_template("index.html", prediction_text="The Predicted Crop is {}".format(prediction[0]))
